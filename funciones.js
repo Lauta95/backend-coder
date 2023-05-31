1ad0d7600d697995e6d2a5aefe6dca10a56caf0e
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-31 20:41:32 -0300

    se quitan archivos innecesarios

---
+++ funciones.js
@@ -0,0 +1,14 @@
+class ProductManager {
+
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
+        this.events = []
+    }
+
+
+}
