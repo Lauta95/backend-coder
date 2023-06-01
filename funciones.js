227eb99dcc42cb14c02ceb7637fbccf91b7ec2e1
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-05-31 21:18:19 -0300

    se agrega un metodo a la clase para agregar un nuevo producto

--- funciones.js
+++ funciones.js
@@ -1,14 +1,32 @@
 class ProductManager {
-
-    constructor(title, description, price, thumbnail, code, stock) {
-        this.title = title;
-        this.description = description;
-        this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
-        this.stock = stock;
-        this.events = []
+    constructor() {
+        this.products = [];
     }
 
+    addProduct = (title, description, price, thumbnail, code, stock) => {
+        const agregar = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log('Error: todos los campos son obligatorios');
+        } else {
+            console.log('OK');
+            this.products.push(agregar);
+        }
+    }
 
+    getProducts() { return this.products }
+
+    getProductById() {
+
+    }
 }
+
+const producto = new ProductManager()
+producto.addProduct('titulo: asd', 'description: dsa', 'precio', 'thumbnail', 'code', 'asdsa')
+// console.log(producto.title);
