ca7967e18f33ed492817a54cd1e2e03570357252
Lautaro Ezequiel Sanabria <lautaro95ss@gmail.com>
2023-06-03 08:55:02 -0300

    se agrega un id

--- funciones.js
+++ funciones.js
@@ -4,9 +4,16 @@ class ProductManager {
         this.products = [];
     }
 
+    getNextId = () => {
+        const count = this.products.length;
+        const nextId = (count > 0) ? this.events[count - 1].id + 1 : 1;
+        return nextId;
+    }
+
     addProduct = (title, description, price, thumbnail, code, stock) => {
 
         const agregar = {
+            id: this.getNextId(),
             title,
             description,
             price,
@@ -17,16 +24,18 @@ class ProductManager {
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.log('Error: todos los campos son obligatorios');
         } else {
-            console.log('OK');
             this.products.push(agregar);
         }
     }
 
-    getProducts() { return this.products }
+    getProducts = () => { return this.products }
 
-    getProductById() {
-    }
 }
 
 const producto = new ProductManager()
-producto.addProduct('titulo', 'description: dsa', 'precio', 'thumbnail', 'code', 'numeroDeStock')
+producto.addProduct('titulo', 'description: dsa', 'precio', 'thumbnail', 'code', 40)
+console.log(producto.getProducts());
+
+const producto2 = new ProductManager()
+producto2.addProduct('titulo2', 'description: dsa', 'dsawe', 'thumbnail', 23, 20,)
+console.log(producto2.getProducts());
